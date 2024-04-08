07df965e3f74616b87e3361dfea8a782f7379add
Lorenzo Ravaglia <lorenzo@ravaglia.xyz>
2024-04-08 09:02:00 -0400

    feat: add yaml output

--- tools/__main__.py
+++ tools/__main__.py
@@ -1,14 +1,27 @@
-import click
+import rich_click as click
 from tools.uwp import UWP
 from tools.subsector import Subsector
 from rich.console import Console
-from rich.table import Table
+from enum import Enum
 
 console = Console()
 
+class Format(str, Enum):
+    TABLE = "table"
+    YAML = "yaml"
+
+class Context:
+    format: Format
+
+    def __init__(self, format: Format) -> None:
+        self.format = format
+
+
 @click.group()
-def main():
-    pass
+@click.option("--format", type=click.Choice(Format), default=Format.TABLE)
+@click.pass_context
+def main(ctx, format):
+    ctx.obj = Context(format)
 
 @main.command()
 def uwp():
@@ -16,16 +29,26 @@ def uwp():
     console.print(uwp)
 
 @main.command()
-def subsector():
+@click.pass_context
+def subsector(ctx):
     subsector = Subsector()
-    table = Table()
 
-    table.add_column("Position", justify="right")
-    table.add_column("UWP", justify="right")
+    if ctx.obj.format == "table":
+        from rich.table import Table
+        table = Table()
 
-    for (pos, uwp) in subsector.worlds:
-        table.add_row(pos, str(uwp))
+        table.add_column("Position", justify="right")
+        table.add_column("UWP", justify="right")
 
-    console.print(table)
+        for (pos, uwp) in subsector.worlds:
+            table.add_row(pos, str(uwp))
+
+        console.print(table)
+    else:
+        import yaml
+        from rich.syntax import Syntax
+        dump = yaml.dump({ "subsector":{ pos:uwp for (pos, uwp) in subsector.worlds }})
+        syntax = Syntax(dump, "yaml", theme="ansi_dark")
+        console.print(syntax)
 
 main()
